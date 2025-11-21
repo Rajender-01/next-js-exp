372fc7ee58e7339c1e4ca12b5d7eec937f3b9482
Rajender kumar <ft.rajender@gmail.com>
2025-11-21 22:03:29 +0530

    implement v2 recaptcha without verification

---
+++ src/app/contact/page.js
@@ -0,0 +1,59 @@
+"use client";
+import { useRef, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+
+const Page = () => {
+  const [isToken, setIsToken] = useState(null);
+  const [formData, setFormData] = useState({ text: "", email: "" });
+  const recaptchaRef =useRef();
+
+  const onChange = (token) => {
+    setIsToken(token);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const recaptchaValue = recaptchaRef.current.getValue();
+    console.log("recaptchaValue", recaptchaValue);
+  };
+  return (
+    <section className="w-full h-dvh flex items-center justify-center bg-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-md bg-white p-8 rounded-2xl shadow-xl flex flex-col space-y-5"
+      >
+        <h2 className="text-2xl font-semibold text-gray-800 text-center">
+          Contact Form
+        </h2>
+        <input
+          type="text"
+          placeholder="Your Name"
+          value={formData.text}
+          onChange={(e) => setFormData((prev) => ({...prev, text: e.target.value}))}
+          className="border border-gray-300 p-3 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+        />
+        <input
+          type="email"
+          value={formData.email}
+          onChange={(e) => setFormData((prev) => ({...prev, email: e.target.value}))}
+          placeholder="Your Email"
+          className="border border-gray-300 p-3 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+        />
+        <ReCAPTCHA
+          ref={recaptchaRef}
+          sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_V2_SITE_KEY}
+          onChange={onChange}
+        />
+        <button
+          type="submit"
+          className="px-4 py-3 bg-blue-600 disabled:bg-gray-400 text-white rounded-lg font-medium hover:bg-blue-700 transition"
+          disabled={!isToken}
+        >
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Page;
